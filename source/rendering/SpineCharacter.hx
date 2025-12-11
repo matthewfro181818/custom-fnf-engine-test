0613877a4d7598b19310e758c52c132d7ae0f80c
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-11 10:48:37 -0500

    Create SpineCharacter.hx

---
+++ source/rendering/SpineCharacter.hx
@@ -0,0 +1,12 @@
+package objects;
+
+class SpineCharacter extends Character {
+    public function new(json:Dynamic) {
+        super();
+        // load using SpineLoader
+    }
+
+    override function playAnim(name:String):Void {
+        // spine animation switching
+    }
+}

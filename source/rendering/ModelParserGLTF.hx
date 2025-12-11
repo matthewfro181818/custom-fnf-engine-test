dbd1f3a923c461648fcc9b527a6c06f6d9eb2d1d
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-11 11:28:39 -0500

    Create ModelParserGLTF.hx

---
+++ source/rendering/ModelParserGLTF.hx
@@ -0,0 +1,8 @@
+package rendering;
+
+class ModelParserGLTF {
+    public static function parse(path:String):Model3D {
+        trace("[GLTF] Parser placeholder — extend later");
+        return new Model3D();
+    }
+}

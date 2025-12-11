dba923c9d02a21975e17e41f5b74549e37dbef87
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-11 11:30:04 -0500

    Update Stage3DLayer.hx

--- source/stages/Stage3DLayer.hx
+++ source/stages/Stage3DLayer.hx
@@ -1,21 +1,25 @@
 package stages;
 
+import rendering.Model3D;
 import rendering.ModelLoader;
 import rendering.ModelRenderer;
+import openfl.display.Sprite;
 
 class Stage3DLayer extends StageLayer {
-    public var model:Dynamic;
+    public var model:Model3D;
+    public var sprite:Sprite;
 
-    public function new(path:String) {
+    public function new(modelPath:String) {
         super();
-        model = ModelLoader.load(path);
+        model = ModelLoader.load(modelPath);
+        sprite = new Sprite();
     }
 
     override public function instantiate(ps:PlayState) {
-        // 3D models do not use FlxSprite
+        ps.add(sprite);
     }
 
     override public function update(elapsed:Float) {
-        ModelRenderer.render(model);
+        ModelRenderer.render(model, sprite.graphics);
     }
 }

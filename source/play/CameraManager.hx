e4e44dcdab7554f84acc7876b2b9bc636e7c296d
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-11 11:00:57 -0500

    Create CameraManager.hx

---
+++ source/play/CameraManager.hx
@@ -0,0 +1,4 @@
+public function addZoom(amt:Float, dur:Float) { ... }
+public function tweenTo(x:Float, y:Float) { ... }
+public function moveTo(x:Float, y:Float, t:Float) { ... }
+public function focusCharacter(id:String) { ... }

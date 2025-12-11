1d267cf3cde831ebe6ce40055cef42873da2b8c9
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-11 11:47:30 -0500

    Create import.hx

---
+++ source/import.hx
@@ -0,0 +1,35 @@
+#if !macro
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.group.FlxGroup;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+import flixel.ui.FlxButton;
+
+import openfl.Assets;
+import openfl.display.Sprite;
+import openfl.geom.Vector3D;
+import openfl.geom.Matrix3D;
+
+import backend.*;
+import backend.audio.*;
+import backend.chart.*;
+import backend.events.*;
+import backend.scripts.*;
+
+import play.*;
+import play.ui.*;
+import play.script.*;
+import play.notes.*;
+
+import objects.*;
+import objects.notes.*;
+import rendering.*;
+import stages.*;
+
+import editors.*;
+import editors.ui.*;
+#end

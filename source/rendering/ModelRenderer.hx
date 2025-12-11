8963f194905637cd0ee4c8f03ba47fc6716daaed
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-11 11:29:23 -0500

    Update ModelRenderer.hx

--- source/rendering/ModelRenderer.hx
+++ source/rendering/ModelRenderer.hx
@@ -1,7 +1,25 @@
-package rendering;
+package stages;
 
-class ModelRenderer {
-    public static function render(model:Dynamic) {
-        // Stage3D / GL draw code
+import rendering.Model3D;
+import rendering.ModelLoader;
+import rendering.ModelRenderer;
+import openfl.display.Sprite;
+
+class Stage3DLayer extends StageLayer {
+    public var model:Model3D;
+    public var sprite:Sprite;
+
+    public function new(modelPath:String) {
+        super();
+        model = ModelLoader.load(modelPath);
+        sprite = new Sprite();
+    }
+
+    override public function instantiate(ps:PlayState) {
+        ps.add(sprite);
+    }
+
+    override public function update(elapsed:Float) {
+        ModelRenderer.render(model, sprite.graphics);
     }
 }
